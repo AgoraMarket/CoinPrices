d92ee148c08da8d8678b5fda99d73371b0f02649
Anekdotin <eddwinn@gmail.com>
2021-04-20 12:12:07 -0400

    Added files

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from app import db
 
 
-class BchPrices(db.Model):
-    __tablename__ = 'prices_bch'
-    __bind_key__ = 'avengers'
-    __table_args__ = {"schema": "avengers_main"}
+class btc_cash_Prices(db.Model):
+    __tablename__ = 'prices_btc_cash'
+    __bind_key__ = 'agora'
+    __table_args__ = {"schema": "public", 'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    currency = db.Column(db.TEXT)
     price = db.Column(db.DECIMAL(50, 2))
-
-db.create_all()
-db.session.commit()
+    currency_id = db.Column(db.INTEGER)
+    percent_change_twentyfour = db.Column(db.DECIMAL(50, 2))
---
+++ coin_prices.py
@@ -0,0 +1,57 @@
+from pycoingecko import CoinGeckoAPI
+from app import db
+from app.models import BchPrices
+
+
+def get_prices_coins():
+    cg = CoinGeckoAPI()
+
+
+    # USD
+    bch_data_usd = BchPrices.query.filter_by(id=1).first()
+
+    bch_usd = cg.get_price(ids='bitcoin-cash', vs_currencies='usd')
+    btc_cash_price_usd = (bch_usd['bitcoin-cash']['usd'])
+    bch_data_usd.price = btc_cash_price_usd
+    db.session.add(bch_data_usd)
+
+
+    # EUR
+    bch_data_eur = BchPrices.query.filter_by(id=2).first()
+
+    bch_eur = cg.get_price(ids='bitcoin-cash', vs_currencies='eur')
+    btc_cash_price_eur = (bch_eur['bitcoin-cash']['eur'])
+    bch_data_eur.price = btc_cash_price_eur
+    db.session.add(bch_data_eur)
+
+
+    # JPY
+    bch_data_jpy = BchPrices.query.filter_by(id=3).first()
+    bch_jpy = cg.get_price(ids='bitcoin-cash', vs_currencies='jpy')
+    btc_cash_price_jpy = (bch_jpy['bitcoin-cash']['jpy'])
+    bch_data_jpy.price = btc_cash_price_jpy
+    db.session.add(bch_data_jpy)
+
+
+    # CAD
+    bch_data_cad = BchPrices.query.filter_by(id=4).first()
+
+    bch_cad = cg.get_price(ids='bitcoin-cash', vs_currencies='cad')
+    btc_cash_price_cad = (bch_cad['bitcoin-cash']['cad'])
+    bch_data_cad.price = btc_cash_price_cad
+    db.session.add(bch_data_cad)
+
+
+    # GBP
+    bch_data_gbp = BchPrices.query.filter_by(id=5).first()
+    bch_gbp = cg.get_price(ids='bitcoin-cash', vs_currencies='gbp')
+    btc_cash_price_gbp = (bch_gbp['bitcoin-cash']['gbp'])
+    bch_data_gbp.price = btc_cash_price_gbp
+    db.session.add(bch_data_gbp)
+
+
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    get_prices_coins()
