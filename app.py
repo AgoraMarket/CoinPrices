4eb894dbe47ae08a141f1423d7fa9c783057de4d
Anekdotin <eddwinn@gmail.com>
2022-12-03 16:50:33 -0500

    Added prod files changed debug dockerignore

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ HOST = '0.0.0.0'
 
 if __name__ == '__main__':
         app.run(
+                debug=True,
                 host=HOST,
                 port=PORT,
                 use_reloader=True
