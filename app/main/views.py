8974813211bb21d7bc1fa133be0d871f227c6dd3
Anekdotin <eddwinn@gmail.com>
2023-01-01 14:03:22 -0500

    Small bug in coin prices.  Cleanup.  Working.

--- app/main/views.py
+++ app/main/views.py
@@ -25,12 +25,12 @@ def get_daemon_status():
 @app.route('/price', methods=['GET'])
 def priceofit():
     """
-    Gets the price of xmr in 5 diff currencies
+    Gets the price of xmr,bch,btc in all currencies
     :return:
     """
     
     cg = CoinGeckoAPI()
-    print("hello")
+
     get_price_json = cg.get_price(ids='bitcoin, bitcoin-cash, monero', vs_currencies='usd,\
                                   php, chf, cad, thb, dkk,sek,sgd,nok,brl,ils,inr,zar,\
                                   hkd,jpy,huf,mxn,cny,aud,pln,gbp,try,krw,idr,nzd,myr,eur,czk')
@@ -40,88 +40,6 @@ def priceofit():
     btc_price.get_prices_coins_btc(jsonprices=get_price_json)
     xmr_price.get_prices_coins_xmr(jsonprices=get_price_json)
     
-    
-
     db.session.commit()
     return jsonify("success")
 
-
-@app.route('/xmr', methods=['GET'])
-def get_xmr_price():
-    """
-    Gets the price of xmr in 5 diff currencies
-    :return:
-    """
-    cg = CoinGeckoAPI()
-
-    get_price_json = cg.get_price(ids='bitcoin, bitcoin-cash, monero',
-                                  vs_currencies='usd, php, chf, cad, \
-                                  thb, dkk,sek,sgd,nok,brl,ils,inr,zar,hkd,\
-                                  jpy,huf,mxn,cny,aud,pln,gbp,try,\
-                                  krw,idr,nzd,myr,eur,czk')
-    try:
-        
-        xmr_price.get_prices_coins_xmr(jsonprices=get_price_json)
-        
-        return jsonify({
-            "status": 'Updated XMR Prices',
-        })
-        
-    except Exception as e:
-        return jsonify({
-            "status": str(e),
-        })
-
-
-@app.route('/btc', methods=['GET'])
-def get_btc_price():
-    """
-    Gets the price of btc in 5 diff currencies
-    :return:
-    """
-    cg = CoinGeckoAPI()
-
-    get_price_json = cg.get_price(ids='bitcoin, bitcoin-cash, monero',
-                                  vs_currencies='usd, php, chf, cad,\
-                                  thb, dkk,sek,sgd,nok,brl,ils,inr,zar,\
-                                  hkd,jpy,huf,mxn,cny,aud,pln,gbp,try,krw,\
-                                  idr,nzd,myr,eur,czk')
-    try:
-        
-        btc_price.get_prices_coins_btc(jsonprices=get_price_json)
-        
-        return jsonify({
-            "status": 'Updated BTC Prices',
-        })
-        
-    except Exception as e:
-        return jsonify({
-            "status": 'Failed to update BTC Prices',
-        })
-
-
-@app.route('/bch', methods=['GET'])
-def get_bch_price():
-    """
-    Gets the price of bch in 5 diff currencies
-    :return:
-    """
-    cg = CoinGeckoAPI()
-
-    get_price_json = cg.get_price(ids='bitcoin, bitcoin-cash, monero',
-                                  vs_currencies='usd, php, chf, cad, thb, \
-                                  dkk,sek,sgd,nok,brl,ils,inr,zar,hkd,\
-                                  jpy,huf,mxn,cny,aud,pln,gbp,try,krw,\
-                                  idr,nzd,myr,eur,czk')
-    try:
-        
-        bch_price.get_prices_coins_bch(jsonprices=get_price_json)
-        
-        return jsonify({
-            "status": 'Updated BCH Prices',
-        })
-        
-    except Exception as e:
-        return jsonify({
-            "status": 'Failed to update BCH Prices',
-        })
