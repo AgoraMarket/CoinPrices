d3431a7fd3ff725408ea395ffa7bd6018bc7d919
Anekdotin <eddwinn@gmail.com>
2023-03-13 08:23:31 -0400

    Tweaks to docker for dynamic deployment

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,6 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 session = sessionmaker()
 
-check_enviroment = ApplicationConfig.CURRENT_SETTINGS
-print(f"starting server with {check_enviroment} settings")
-
-
-
-
-
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
@@ -40,7 +33,7 @@ app.json_encoder = DecimalEncoder
 
 
 
-session.configure(bind=ApplicationConfig.SQLALCHEMY_DATABASE_URI_0)
+session.configure(bind=ApplicationConfig.SQLALCHEMY_DATABASE_URI)
 db = SQLAlchemy(app)
 server_session = Session(app)
 ma = Marshmallow(app)
